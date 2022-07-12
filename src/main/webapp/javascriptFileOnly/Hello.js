ec8a9e987169e7f32b255f89cfd863e28f460ed2
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-07-12 13:00:27 +0530

    updated

--- src/main/webapp/javascriptFileOnly/Hello.js
+++ src/main/webapp/javascriptFileOnly/Hello.js
@@ -2,3 +2,8 @@
  * 
  */
  console.log("hello world")
+ var number = 12;
+ var num1 = 123;
+ console.log(num1+number);
+ 
+ 
---
+++ src/main/webapp/javascriptFileOnly/add.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+ 
+const num1 = parseInt(prompt('Enter the first number ')); 
+const  num2 =parseInt(prompt('Enter the second number ')); 
+console.log("the sum of the two number is"+ num1+num2);
+console.log("the subtraction of the two number is " + num1-num2);
+console.log("the multiplication of the two number is " + num1*num2);
+console.log("the division of the two number is " + num1/num2);
