dff34bca41103be8087da0a1e50ae6eed6d645c5
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-07-22 11:20:18 +0530

    coded

--- src/main/webapp/javascriptFileOnly/add.js
+++ src/main/webapp/javascriptFileOnly/add.js
@@ -2,9 +2,7 @@
  * 
  */
  
-const num1 = parseInt(prompt('Enter the first number ')); 
-const  num2 =parseInt(prompt('Enter the second number ')); 
-console.log("the sum of the two number is"+ num1+num2);
-console.log("the subtraction of the two number is " + num1-num2);
-console.log("the multiplication of the two number is " + num1*num2);
-console.log("the division of the two number is " + num1/num2);
+const num1 = parseInt(prompt("Enter the first Number"));
+const num2 = parseInt(prompt("Enter the second  Number"));
+const sum = num1+num2;
+console.log(sum);
